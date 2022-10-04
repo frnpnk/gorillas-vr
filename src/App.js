9a256588131df218f184fbdbcfc4a24e7d2c9464
frnpnk <franciscogerardi@gmail.com>
2022-10-04 18:33:51 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import BoxVr from "./components/little_boxes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Gorillas Vr </header>
+      <Canvas >
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <BoxVr position={[-1.2, 0, 0]} />
+        <BoxVr position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
   );
 }
